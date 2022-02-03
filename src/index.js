6848355ee7ff64a4a6cabcef3b9982655e52794d
softodrom <cristian.guba@gmail.com>
2022-02-03 16:19:00 +0100

    hello

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/Counter";
+
+
+const element = <h1>Hello world!</h1>;
+console.log(element);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+ReactDOM.render(<Counter />, document.getElementById("root"));
+// ReactDOM.render(element, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
